878972e737924c55e2ea1e837d58a90d96622fff
Aman <Amanchand1225@gmail.com>
2025-06-27 22:38:28 +0530

    about page updated commit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@
 const Footer = () => {
   return (
     <footer className="flex items-center text-center justify-center border-t border-zinc-200 p-4 dark:border-zinc-700">
-      &copy; {new Date().getFullYear()} My Portfolio
+      &copy; {new Date().getFullYear()} My Portfolio | Aman Raiswal
     </footer>
   );
 };
